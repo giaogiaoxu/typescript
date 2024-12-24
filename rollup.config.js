216e134f3f542311799f7714712cd927edd0dd57
xulongji <you@example.com>
2024-12-24 17:26:41 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import ts from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import path, { format } from "path";
+import { fileURLToPath } from "url";
+const __fileName = fileURLToPath(import.meta.url); //当前配置文件的绝对路径
+const __dirname = path.dirname(__fileName); //当前配置文件所在目录的绝对路径（不包含文件名）
+export default {
+  input: "src/index.ts",
+  output: {
+    file: path.resolve(__dirname, "dist/bundle.js"),
+    format: "iife",
+  },
+  plugin: [
+    nodeResolve({
+      extensions: [".js", ".ts"],
+    }),
+    ts({
+      tsconfig: path.resolve(__dirname, "tsconfig.json"),
+    }),
+  ],
+};
