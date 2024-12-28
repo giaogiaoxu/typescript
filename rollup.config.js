e640ec46e8fe5b50c0ba7cfccd6e88320c3ef5d1
xulongji <you@example.com>
2024-12-28 20:55:37 +0800

    类型介绍

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,16 @@ import ts from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import path, { format } from "path";
 import { fileURLToPath } from "url";
+
 const __fileName = fileURLToPath(import.meta.url); //当前配置文件的绝对路径
 const __dirname = path.dirname(__fileName); //当前配置文件所在目录的绝对路径（不包含文件名）
 export default {
-  input: "src/index.ts",
+  input: "./src/index.ts",
   output: {
     file: path.resolve(__dirname, "dist/bundle.js"),
     format: "iife",
   },
-  plugin: [
+  plugins: [
     nodeResolve({
       extensions: [".js", ".ts"],
     }),
